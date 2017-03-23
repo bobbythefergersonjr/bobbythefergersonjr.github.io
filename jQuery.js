cf3c79f8f05847699551634a580f2422f9a4970f
Royxgh <ghandourroy@gmail.com>
2017-03-23 08:13:37 +0400

    starting the planned website

--- jQuery.js
+++ jQuery.js
@@ -3,13 +3,20 @@ $(document).ready(function() {
 		if($(document).scrollTop() >= 100) {
     		$('#foreground').addClass('hold');
 
-    		if ($(document).scrollTop() >= 150) {
-	    		$('.left img').animate({left: '0px', opacity: '1'});
-	    		$('.right img').animate({right: '0px', opacity: '1'});
-    		} 
+    		
+    		/*
+    		if ($(document).scrollTop() >= 200) {
+    			$('.left img').animate({left: '-400px', opacity: '0'});
+	    		$('.right img').animate({right: '-400px', opacity: '0'});
+    		}*/
   	 	}
     	else {
     		$('#foreground').removeClass('hold');
     	}
+
+    	if ($(document).scrollTop() >= 150) {
+	    		$('.left img').animate({left: '0px', opacity: '1'});
+	    		$('.right img').animate({right: '0px', opacity: '1'});
+    	} 
     });
 });
