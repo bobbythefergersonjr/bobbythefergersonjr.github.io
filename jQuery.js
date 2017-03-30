06218f319298e95b09f4499bf0d336fa9593190e
Royxgh <ghandourroy@gmail.com>
2017-03-30 12:55:42 +0400

    Added Screen

--- jQuery.js
+++ jQuery.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
 		if($(document).scrollTop() >= 100) {
-    		$('#foreground').addClass('hold');
-
-    		
-    		/*
-    		if ($(document).scrollTop() >= 200) {
-    			$('.left img').animate({left: '-400px', opacity: '0'});
-	    		$('.right img').animate({right: '-400px', opacity: '0'});
-    		}*/
+    		$('#title-pannel').addClass('hold');
   	 	}
     	else {
-    		$('#foreground').removeClass('hold');
+    		$('#title-pannel').removeClass('hold');
     	}
 
     	if ($(document).scrollTop() >= 150) {
-	    		$('.left img').animate({left: '0px', opacity: '1'});
-	    		$('.right img').animate({right: '0px', opacity: '1'});
+	    	$('.left img').animate({left: '0px', opacity: '1'});
+	    	$('.right img').animate({right: '0px', opacity: '1'});
     	} 
+
+        if ($(document).scrollTop() >= 700) {
+            $('#screen-pannel').addClass('hold');
+        } 
+        else {
+            $('#screen-pannel').removeClass('hold');
+        }
     });
 });
