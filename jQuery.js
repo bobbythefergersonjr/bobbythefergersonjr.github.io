0158d798b7218b30729c67599a295d139784b972
Royxgh <ghandourroy@gmail.com>
2017-04-14 15:10:58 +0400

    Boi

--- jQuery.js
+++ jQuery.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    var view_width = $(window).width();
+    var view_height = $(window).height();
+
 	$(window).scroll(function() {
 		if($(document).scrollTop() >= 100) {
     		$('#title-pannel').addClass('hold');
@@ -8,15 +11,39 @@ $(document).ready(function() {
     	}
 
     	if ($(document).scrollTop() >= 150) {
-	    	$('.left img').animate({left: '0px', opacity: '1'});
-	    	$('.right img').animate({right: '0px', opacity: '1'});
+	    	$('#left').animate({left: '0px', opacity: '1'});
+	    	$('#right').animate({right: '0px', opacity: '1'});
     	} 
 
         if ($(document).scrollTop() >= 700) {
-            $('#screen-pannel').addClass('hold');
+            $('#screen-pannel').css({"position": "fixed", "paddingTop": "100px"});
+            if ($(document).scrollTop() >= 2.5*view_height) {
+                $('#screen-pannel').css({"position": "absolute", "paddingTop": "calc(250vh + 100px)"});
+            }
         } 
         else {
-            $('#screen-pannel').removeClass('hold');
+            $('#screen-pannel').css({"position": "absolute", "paddingTop": "800px"});
         }
+        
+        if ($(document).scrollTop() >= 3*view_height) {
+            $('#pic-1').css("display", "none");
+            $('#pic-2').css("display", "block");
+        }
+        else {
+             $('#pic-1').css("display", "block");
+            $('#pic-2').css("display", "none");
+        }
+
+        if ($(document).scrollTop() >= 3.5*view_height) {
+            $('#strap-pannel').css({"position": "fixed", "paddingTop": "100px"});
+            /*
+            if ($(document).scrollTop() >= 2.5*view_height) {
+                $('#screen-pannel').css({"position": "absolute", "paddingTop": "calc(250vh + 100px)"});
+            }*/
+        } 
+        else {
+            $('#strap-pannel').css({"position": "absolute", "paddingTop": "calc(350vh + 100px)"});
+        }
+
     });
 });
