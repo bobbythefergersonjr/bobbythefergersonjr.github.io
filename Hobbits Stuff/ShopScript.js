963a9fc0d2a178ff8353b41d0d9a8ec3593cb0e3
DarkD3stroyer <dia14333@diastudents.com>
2017-04-24 13:20:24 +0400

    shop update, cuz idk y it wasnt updated alredy, gud english btw.

--- Hobbits Stuff/ShopScript.js
+++ Hobbits Stuff/ShopScript.js
@@ -1,20 +1,38 @@
-function customerNameLoad() {
-var customerName;
-customerName=prompt('What is Your Name?','');
-document.getElementById('customerName').innerHTML = customerName;
-alert('Welcome To The Ninvento Store, '+customerName+'!');
+setInterval(function () {
+        plusSlides(1);
+    }, 3000);
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-function open(evt, TabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(TabName).style.display = "block";
-    evt.currentTarget.className += " active";
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-document.getElementById("defaultOpen").click();
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/*
+var pic = document.getElementById('imageid');
+function grey() {
+  pic.src = "Images\pls.jpg";
+}
+function blue() {
+  pic.src = "Images\pA-header1.jpg";
+}
+document.getElementById('btn1').addEventListener('click', grey);
+document.getElementById('btn2').addEventListener('click', blue);
+*/
